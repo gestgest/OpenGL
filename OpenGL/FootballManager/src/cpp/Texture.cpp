6d22c5e45d3d76c00c9c319e89c5c3e847efc1cd
gestgest <milkan660@naver.com>
2024-05-29 20:35:51 +0900

    텍스처

---
+++ OpenGL/FootballManager/src/cpp/Texture.cpp
@@ -0,0 +1,46 @@
+#include "../header/Texture.h"
+
+Texture::Texture(const std::string& path) {
+	this->filePath = path;
+	localBuffer = nullptr;
+	this->width = 0;
+	this->height = 0;
+	this->pixel_bit = 0;
+
+	//GL 형식상 png의 텍스처를 거꾸로 만들어야 함 [상하반전]
+	//GL은 왼쪽 하단에서 시작하기 때문, png는 왼쪽 상단
+	stbi_set_flip_vertically_on_load(1); //상하반전 함수
+	localBuffer = stbi_load(path.c_str(), &width, &height, &pixel_bit, 4); //4 -> rgba, 채널 4개
+	
+	GLCHECK(glGenTextures(1, &id));
+	GLCHECK(glBindTexture(GL_TEXTURE_2D, id));
+
+	//텍스처 파라미터 넣는 함수. 필수로 4개의 파라미터를 지정해야 함 = > 안하면 검은색 질감으로 보임
+	GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+	GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)); //x
+	GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)); //y
+
+	//타겟, 레벨(다중 레벨이면 1이상), 내부포멧, width, height, border, 외부포멧, 타입, 픽셀버퍼(nullptr)
+	GLCHECK(glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer))
+}
+
+Texture::~Texture() {
+
+}
+
+void Texture::bind(unsigned int slot = 0) const{
+
+}
+
+void Texture::unBind() {
+
+}
+
+inline int Texture::getWidth() const {
+	return this->width;
+}
+
+inline int Texture::getHeight() const {
+	return this->height;
+}
--- OpenGL/FootballManager/src/header/Texture.h
+++ OpenGL/FootballManager/src/header/Texture.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "Renderer.h"
+#include "DebugRenderer.h"
+#include "stb/stb_image.h"
 
 class Texture {
 private:
 	unsigned int id;
 	std::string filePath;
 	unsigned char* localBuffer;
-	int width, height, bpp;
+	int width, height, pixel_bit; //bpp : 1 픽셀당 비트
 public:
 	Texture(const std::string& path);
 	~Texture();
 
-	void bind() const;
+	void bind(unsigned int slot = 0) const;
 	void unBind();
 
 	inline int getWidth() const;
