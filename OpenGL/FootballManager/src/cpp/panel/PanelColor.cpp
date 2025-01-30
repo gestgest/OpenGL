e315411e22f46a39e5ad7d16b3d050cd41a372f7
gestgest <87427692+gestgest@users.noreply.github.com>
2025-01-31 00:07:22 +0900

    Update PanelColor.cpp

--- OpenGL/FootballManager/src/cpp/panel/PanelColor.cpp
+++ OpenGL/FootballManager/src/cpp/panel/PanelColor.cpp
@@ -1,9 +1,14 @@
 #include "../../header/panel/PanelColor.h"
 
+
+//opengl 강의에서의 TestTexture2D
 namespace panel
 {
 	PanelColor::PanelColor()
 	{
+		//컬러들은 PanelColor 안에 있음
+
+		//뭐 메인 소스들 다 여기에 넣던데?
 		color[0] = 0.0f;
 		color[1] = 0.0f;
 		color[2] = 0.0f;
